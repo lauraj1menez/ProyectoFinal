cf38cd473c538b4c1455d98538fb2ace8154c4d7
Laura Jiménez <88410024+lauraj1menez@users.noreply.github.com>
2024-08-07 16:50:16 -0400

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 test('Verifica el contenido del h3', () => {
     const html = fs.readFileSync('index.html', 'utf8');
-    const h1Match = html.match(/<h3[^>]*>([^<]+)<\/h3>/);
-    const h1Text = h3Match ? h3Match[1] : null;
+    const h3Match = html.match(/<h3[^>]*>([^<]+)<\/h3>/);
+    const h3Text = h3Match ? h3Match[1] : null;
     expect(h3Text).toBe('Hola Mundo');
 });
